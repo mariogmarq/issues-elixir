7869b6ece89f336b8c5c49afae74b3597a7ddb0a
mariogmarq <mariogarciamarq@gmail.com>
2022-12-18 11:34:55 +0100

    refactor cli

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -17,13 +17,12 @@ defmodule Issues.CLI do
   It will return `{user, project, count}` or `:help`
   """
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
-
-    case parse do
-      {[help: true], _, _} -> :help
-      {_, [user, project], _} -> {user, project, @default_count}
-      {_, [user, project, count], _} -> {user, project, String.to_integer count}
-      _ -> :help
-    end
+    OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    |> elem(1)
+    |> args_to_internal_representation()
   end
+
+  defp args_to_internal_representation([user, project]), do: {user, project, @default_count}
+  defp args_to_internal_representation([user, project, count]), do: {user, project, String.to_integer count}
+  defp args_to_internal_representation(_), do: :help
 end
